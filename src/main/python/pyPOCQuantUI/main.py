e525bf2ce3d5e494eb2c6d2a3be8e3ad514d21e2
Andreas P. Cuny <andreas.cuny@bsse.ethz.ch>
2020-08-20 12:25:43 +0200

    Add check if tesseract is on path, if not asks user to install it first

--- src/main/python/pyPOCQuantUI/main.py
+++ src/main/python/pyPOCQuantUI/main.py
@@ -1,34 +1,59 @@
 from fbs_runtime.application_context.PyQt5 import ApplicationContext, cached_property
 
 import sys
+import shutil
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMessageBox
 from ui.mainwindow import MainWindow
 
 
 class AppContext(ApplicationContext):
+
     def run(self):
-        self.window.resize(1340, 680)
-        self.window.show()
-        # self.window.strip_action.setIcon(QIcon(self.get_resource("img/strip-01.png")))
-        self.window.sensor_action.setIcon(QIcon(self.get_resource("img/sensor-01.png")))
-        self.window.delete_items_action.setIcon(QIcon(self.get_resource("img/sensor_trash-01.png")))
-        self.window.mirror_v_action.setIcon(QIcon(self.get_resource("img/mirror_v-01.png")))
-        self.window.mirror_h_action.setIcon(QIcon(self.get_resource("img/mirror_h-01.png")))
-        self.window.rotate_cw_action.setIcon(QIcon(self.get_resource("img/rotate_cw-01.png")))
-        self.window.rotate_ccw_action.setIcon(QIcon(self.get_resource("img/rotate_ccw-01.png")))
-        self.window.zoom_out_action.setIcon(QIcon(self.get_resource("img/zoom_out-01.png")))
-        self.window.zoom_in_action.setIcon(QIcon(self.get_resource("img/zoom_in-01.png")))
+
+        cmd = 'tesseract'
+        if self.cmd_exists(cmd):
+            self.window.resize(1340, 680)
+            self.window.show()
+            # self.window.strip_action.setIcon(QIcon(self.get_resource("img/strip-01.png")))
+            self.window.sensor_action.setIcon(QIcon(self.get_resource("img/sensor-01.png")))
+            self.window.delete_items_action.setIcon(QIcon(self.get_resource("img/sensor_trash-01.png")))
+            self.window.mirror_v_action.setIcon(QIcon(self.get_resource("img/mirror_v-01.png")))
+            self.window.mirror_h_action.setIcon(QIcon(self.get_resource("img/mirror_h-01.png")))
+            self.window.rotate_cw_action.setIcon(QIcon(self.get_resource("img/rotate_cw-01.png")))
+            self.window.rotate_ccw_action.setIcon(QIcon(self.get_resource("img/rotate_ccw-01.png")))
+            self.window.zoom_out_action.setIcon(QIcon(self.get_resource("img/zoom_out-01.png")))
+            self.window.zoom_in_action.setIcon(QIcon(self.get_resource("img/zoom_in-01.png")))
+        else:
+            self.show_tesseract_install_dialog()
         return appctxt.app.exec_()
 
     def get_design(self):
         qtCreatorFile = self.get_resource("form.ui")
         return qtCreatorFile
 
+    def cmd_exists(self, cmd):
+        return shutil.which(cmd) is not None
+
     @cached_property
     def window(self):
         ui = self.get_design()
         splash = self.get_resource("img/pyPOCQuantSplash-01.png")
         return MainWindow(ui, splash)
+
+    def show_tesseract_install_dialog(self):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Warning)
+        msg.setTextFormat(Qt.RichText);
+        msg.setText("The tool requires the software TESSERACT to be installed.")
+        msg.setInformativeText("TESSERACT has not been detected please follow this link "
+                               "(<a href='https://tesseract-ocr.github.io/tessdoc/Home.html'>Install TESSERACT</a>) "
+                               "to install tesseract first and make sure it is on the system path. ")
+        msg.setWindowTitle("pyPOCQuantUI :: Warning")
+        msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        retval = msg.exec_()
+        sys.exit(retval)
 
 
 if __name__ == "__main__":
