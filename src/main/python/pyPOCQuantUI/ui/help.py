699111efc752e8b889b72491dd97a08bc5a5839a
Aaron Ponti <aarpon@gmail.com>
2021-01-05 16:59:25 +0100

    Add copyright notice to all source code files.

--- src/main/python/pyPOCQuantUI/ui/help.py
+++ src/main/python/pyPOCQuantUI/ui/help.py
@@ -1,13 +1,14 @@
-# /********************************************************************************
-# * Copyright © 2020, ETH Zurich, D-BSSE, Andreas P. Cuny
-# * All rights reserved. This program and the accompanying materials
-# * are made available under the terms of the GNU Public License v3.0
-# * which accompanies this distribution, and is available at
-# * http://www.gnu.org/licenses/gpl
-# *
-# * Contributors:
-# *     Andreas P. Cuny - initial API and implementation
-# *******************************************************************************/
+#  ********************************************************************************
+#   Copyright © 2020-2021, ETH Zurich, D-BSSE, Andreas P. Cuny & Aaron Ponti
+#   All rights reserved. This program and the accompanying materials
+#   are made available under the terms of the GNU Public License v3.0
+#   which accompanies this distribution, and is available at
+#   http://www.gnu.org/licenses/gpl
+#
+#   Contributors:
+#     * Andreas P. Cuny - initial API and implementation
+#     * Aaron Ponti - initial API and implementation
+#  *******************************************************************************
 
 from PyQt5 import QtGui
 from PyQt5.QtCore import Qt, QSettings
--- src/main/python/pyPOCQuantUI/ui/log.py
+++ src/main/python/pyPOCQuantUI/ui/log.py
@@ -1,3 +1,15 @@
+#  ********************************************************************************
+#   Copyright © 2020-2021, ETH Zurich, D-BSSE, Andreas P. Cuny & Aaron Ponti
+#   All rights reserved. This program and the accompanying materials
+#   are made available under the terms of the GNU Public License v3.0
+#   which accompanies this distribution, and is available at
+#   http://www.gnu.org/licenses/gpl
+#
+#   Contributors:
+#     * Andreas P. Cuny - initial API and implementation
+#     * Aaron Ponti - initial API and implementation
+#  *******************************************************************************
+
 from PyQt5.QtCore import Qt, QMetaObject, Q_ARG, pyqtSlot
 from PyQt5.QtWidgets import QPlainTextEdit
 from PyQt5.QtGui import QTextCursor
