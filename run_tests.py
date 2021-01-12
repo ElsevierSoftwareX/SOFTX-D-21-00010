56d0ab6a2bedbec5b139439b640133b490e97529
Aaron Ponti <aarpon@gmail.com>
2021-01-12 15:08:48 +0100

    Add script run_tests.py.

---
+++ run_tests.py
@@ -0,0 +1,18 @@
+#  ********************************************************************************
+#   Copyright © 2020-2021, ETH Zurich, D-BSSE, Andreas P. Cuny & Aaron Ponti
+#   All rights reserved. This program and the accompanying materials
+#   are made available under the terms of the GNU Public License v3.0
+#   which accompanies this distribution, and is available at
+#   http://www.gnu.org/licenses/gpl
+#
+#   Contributors:
+#     * Andreas P. Cuny - initial API and implementation
+#     * Aaron Ponti - initial API and implementation
+#  *******************************************************************************
+
+import unittest
+from pypocquant import tests
+
+# Run all unit tests
+suite = unittest.TestLoader().loadTestsFromModule(tests)
+unittest.TextTestRunner().run(suite)
